918f9e42ddf329f359c09362751701ebc9456b22
Your Name <you@example.com>
2020-05-27 10:07:47 +0800

    第一次提交

---
+++ src/main/java/com/bw/bean/Hello.java
@@ -0,0 +1,11 @@
+package com.bw.bean;
+
+import org.junit.Test;
+
+public class Hello {
+	
+	@Test
+	public void test01(){
+		System.out.println(2222222);
+	}
+}
---
+++ src/test/java/com/bw/test/TestHello.java
@@ -0,0 +1,11 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+public class TestHello {
+	
+	@Test
+	public void test01(){
+		System.out.println(111111);
+	}
+}
